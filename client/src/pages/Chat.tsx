66dc0fdb88158176edbe1b36f6ae0687e55d7af8
Chirag Kashyap <chiragk1412@gmail.com>
2026-02-09 14:22:09 +0530

    added automatic scrolling to the latest text feature once user opens keyboard on his device

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -121,9 +121,13 @@ const Chat = () => {
         }, 1000);
     }
 
-    useEffect(()=>{
+    const scrollIntooView = () =>{
         // @ts-ignore
         anchor.current?.scrollIntoView({ behavior: "smooth" });
+    }
+
+    useEffect(()=>{
+        scrollIntooView()
     },[messages])
 
     const typingNames = [...typingUsers.values()]
@@ -238,7 +242,7 @@ const Chat = () => {
                 <div
                     className="typing refined glass backdrop-blur-[12px] w-full h-[9%] sm:rounded-b-[1.4rem] flex justify-between items-center p-4 gap-[0.6rem] text-[1rem]">
 
-                    <input onInput={typing} onKeyDown={(e)=>{if(e.key=="Enter") onSend()}} onChange={(e)=>setText(e.target.value)} value={text} id="inputText" className="glass-other rounded-[5rem] border border-zinc-200 p-4 w-[88%] h-10"
+                    <input onClick={()=>{scrollIntooView()}} onInput={typing} onKeyDown={(e)=>{if(e.key=="Enter") onSend()}} onChange={(e)=>setText(e.target.value)} value={text} id="inputText" className="glass-other rounded-[5rem] border border-zinc-200 p-4 w-[88%] h-10"
                         type="text" placeholder="Type a message..."/>
                     <button onClick={onSend} id="btn"
                         className="bg-green-500 text-white h-10 w-[12%] glass glass-button rounded-[1.4rem] flex justify-center items-center text-center cursor-pointer"><img
